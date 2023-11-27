c3d196b5b8035b0ea860d537ae589ea28120bc54
Tyler K <tylerkruer1@gmail.com>
2023-11-27 09:57:20 -0700

    Commented out logging utility - cant run as CRON

--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
-import { logging } from "./src/utils"
 import { sendSlackMessage } from "./src/slack"
 import {
   getHiltonHotels, 
@@ -7,17 +6,17 @@ import {
 } from "./src/hilton"
 
 async function main() { 
-  logging({ event: "Starting Flight and Hotel Scraper", data: "Starting Flight and Hotel Scraper" })
+  // logging({ event: "Starting Flight and Hotel Scraper", data: "Starting Flight and Hotel Scraper" })
 
   // TODO: Come back to Flights (southwest.ts and american.ts) in about a Month. Prices aren't updated for next year.
   const hiltonHotels = await getHiltonHotels()
-  logging({ event: "Hilton Hotels", data: `Found ${hiltonHotels.data.hotelSummaryOptions.hotels.length}` })
+  // logging({ event: "Hilton Hotels", data: `Found ${hiltonHotels.data.hotelSummaryOptions.hotels.length}` })
 
   const hiltonHotelSummary = await processHiltonHotelsData({ hiltonData: hiltonHotels })
-  logging({ event: "Hilton Hotels Processed Data", data: `Processed ${hiltonHotelSummary.length}` })
+  // logging({ event: "Hilton Hotels Processed Data", data: `Processed ${hiltonHotelSummary.length}` })
   
   const hiltonHotelSummarySaved = await saveHiltonHotelData({ hiltonHotelSummary: hiltonHotelSummary })
-  logging({ event: "Hilton Hotels Saved", data: `Saved Hotel Data` })
+  // logging({ event: "Hilton Hotels Saved", data: `Saved Hotel Data` })
 
   sendSlackMessage({ logString: `Hilton Hotels Processed: ${hiltonHotelSummary.length}`, status: "success", failure: false })
 }
